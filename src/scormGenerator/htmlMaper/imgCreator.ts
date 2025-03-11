0b494be9a95f8d72ba3264b7972517b2d99b1a7b
Alexander Shatalkin <s.shatalkin@mail.ru>
2025-03-11 15:02:36 +0300

    change image sizes to size in metadata instead of 100% width

--- src/scormGenerator/htmlMaper/imgCreator.ts
+++ src/scormGenerator/htmlMaper/imgCreator.ts
@@ -1,7 +1,16 @@
 import { HtmlCreator } from "./htmlCreator";
+import sharp from "sharp";
 
 export default class ImgCreator implements HtmlCreator{
-    generate(element: any): string {
-        return `<img src="${element.url}" style="width: 100%; height: auto;">`;
+    async generate(element: any): Promise<string> {
+            const base64Data = element.url.replace(/^data:image\/\w+;base64,/, '');
+            const imageBuffer = Buffer.from(base64Data, 'base64');
+        
+            const metadata = await sharp(imageBuffer).metadata();
+            const {width, height} = {width:metadata.width, height:metadata.height}
+        
+        return `<img src="${element.url}" style="width: ${width || "100%"}px; height: ${height || "auto"}px;">`;
+
+
     }
 }
