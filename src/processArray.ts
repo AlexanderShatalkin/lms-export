2d593da4e0cf1d6bf3ce1bd2cfb34bcd616af9d0
Alexander Shatalkin <s.shatalkin@mail.ru>
2025-03-12 18:47:00 +0300

    delete comented lines

--- src/processArray.ts
+++ src/processArray.ts
@@ -1,26 +1,6 @@
 import { Prisma, StudyWork, Work, WorkVariant } from "../prisma/client";
 import { WorkWithVariants } from "./interfaces";
 
-// type WorkWithVariants = Prisma.WorkGetPayload<{select: {
-//   id: true,
-//   name: true,
-//   description: true,
-//   course: true,
-//   workVariants: {
-//     select: {
-//       id: true,
-//       name: true,
-//       tasks: true,
-//       base: true,
-//     },
-//   },
-//   studyWork: {
-//     select: {
-//       workType: true,
-//     },
-//   },
-// }}>;
-
 export function GroupWorksByWorkType(works:WorkWithVariants[]):{workType: string, works: WorkWithVariants[]}[]{
 
   const grouped =  works.reduce<Record<string, { workType: string; works: WorkWithVariants[] }>>((acc, work) => {
